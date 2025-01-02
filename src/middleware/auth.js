b40a47926b1f3ac08c7d550148a55861ad7b7bcf
Sandesh <er.sandesh.2022@gmail.com>
2025-01-02 07:48:36 +0545

    html and auth.js files @

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const protect = async (req, res, next) => {
     try {
         let token;
 
-        if (req.headers.authorization && 
+        if (req.headers.authorization &&
             req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
         }
